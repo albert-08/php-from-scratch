a72ff9f75947e3149e9cc4aad4b3ac483c2e6723
Alberto Flores <alberto.floreso94@gmail.com>
2026-04-28 15:08:38 -0600

    tipos datos variables

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+$name = 'Beto';
+
+echo "Hola {$name}";
---
+++ tipos_datos_variables.php
@@ -0,0 +1,28 @@
+<?php
+
+// Tipos de datos y variables
+// En PHP, las variables se definen con el símbolo de dólar ($) seguido del nombre de la variable.
+
+// Tipos de datos comunes en PHP:
+// 1. String: Cadena de texto
+$name = 'Beto';
+
+// 2. Integer: Número entero
+$quantity = 14;
+
+// 3. Float: Número decimal
+$price = 9.99;
+
+// 4. Boolean: Verdadero o falso
+$paymentStatus = false;
+
+// 5. null: Sin valor
+$startDate = null;
+
+var_dump($name);
+var_dump($quantity);
+var_dump($price);
+var_dump($paymentStatus);
+var_dump($startDate);
+
+// echo "Hola {$name}";
