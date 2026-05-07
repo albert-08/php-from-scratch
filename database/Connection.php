b0cc94e986aa0c377409ef6168f89e57ba3857a3
Alberto Flores <alberto.floreso94@gmail.com>
2026-05-07 17:38:32 -0600

    add config

--- database/Connection.php
+++ database/Connection.php
@@ -2,10 +2,14 @@
 
 class Connection
 {
-    public static function start()
+    public static function start($config)
     {
         try {
-            $pdo = new PDO('mysql:host=127.0.0.1;dbname=todos', 'root', '');
+            $pdo = new PDO(
+                "{$config['type']}:host={$config['host']};dbname={$config['database']}",
+                $config['user'],
+                $config['password']
+            );
 
             return $pdo;
         } catch (PDOException $error) {
